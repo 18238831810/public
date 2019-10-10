582504d8e7b8ec89af74c5a970b5622b0a343932
frank <1527583922@qq.com>
2019-10-10 22:48:06 +0800

    导航栏操作

--- crs-security/src/main/java/com/cf/crs/sys/controller/SysMenuController.java
+++ crs-security/src/main/java/com/cf/crs/sys/controller/SysMenuController.java
@@ -8,15 +8,19 @@
 
 package com.cf.crs.sys.controller;
 
+import com.cf.crs.common.constant.Constant;
 import com.cf.crs.sys.entity.SysMenuEntity;
 import com.cf.crs.sys.service.SysMenuService;
 import com.cf.util.http.ResultJson;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -36,6 +40,31 @@ public class SysMenuController {
 	@ApiOperation("菜单栏")
 	public ResultJson<List<SysMenuEntity>> getMenus(){
 		return sysMenuService.getMenus();
+	}
+
+	@GetMapping("/add")
+	@ApiOperation("添加菜单栏")
+	public ResultJson<String> addMenu(SysMenuEntity sysMenuEntity){
+		return sysMenuService.addMenu(sysMenuEntity);
+	}
+
+	@GetMapping("/edit")
+	@ApiOperation("编辑菜单栏")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "id", paramType = "query", required = true, dataType="String") ,
+			@ApiImplicitParam(name = "编辑名称",  paramType = "query",required = true, dataType="String") ,
+	})
+	public ResultJson<String> editMenu(@ApiIgnore String id,@ApiIgnore String name){
+		return sysMenuService.editMenu(id,name);
+	}
+
+	@GetMapping("/delete")
+	@ApiOperation("编辑菜单栏")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "id", paramType = "query", required = true, dataType="String") ,
+	})
+	public ResultJson<String> deleteMenu(@ApiIgnore String id){
+		return sysMenuService.deleteMenu(id);
 	}
 
 
--- crs-security/src/main/java/com/cf/crs/sys/mapper/SysMenuMapper.java
+++ crs-security/src/main/java/com/cf/crs/sys/mapper/SysMenuMapper.java
@@ -23,25 +23,6 @@ import java.util.List;
 @Mapper
 public interface SysMenuMapper extends BaseDao<SysMenuEntity> {
 
-	SysMenuEntity getById(@Param("id") Long id, @Param("language") String language);
-
-	/**
-	 * 查询所有菜单列表
-	 *
-	 * @param type 菜单类型
-	 * @param language 语言
-	 */
-	List<SysMenuEntity> getMenuList(@Param("type") Integer type, @Param("language") String language);
-
-	/**
-	 * 查询用户菜单列表
-	 *
-	 * @param userId 用户ＩＤ
-	 * @param type 菜单类型
-	 * @param language 语言
-	 */
-	List<SysMenuEntity> getUserMenuList(@Param("userId") Long userId, @Param("type") Integer type, @Param("language") String language);
-
 	/**
 	 * 查询用户权限列表
 	 * @param userId  用户ID
@@ -53,10 +34,5 @@ public interface SysMenuMapper extends BaseDao<SysMenuEntity> {
 	 */
 	List<String> getPermissionsList();
 
-	/**
-	 * 根据父菜单，查询子菜单
-	 * @param pid  父菜单ID
-	 */
-	List<SysMenuEntity> getListPid(Long pid);
 
 }
