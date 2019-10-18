824992d4f594e6452c599c718e254272b5c20e62
frank <Frank.han@cf688.net>
2019-10-18 14:33:19 +0800

    考评计划

--- crs-admin/src/main/java/com/cf/crs/entity/CheckPlan.java
+++ crs-admin/src/main/java/com/cf/crs/entity/CheckPlan.java
@@ -1,6 +1,5 @@
 package com.cf.crs.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -14,10 +13,10 @@ import java.io.Serializable;
  **/
 @Data
 @ApiModel(value = "考评计划管理")
-@TableName("city_check_plan")
 public class CheckPlan implements Serializable {
 
-    private Integer id;
+    @ApiModelProperty(value = "1:日报表，2:周报表，3:月报表 ,4:年报表")
+    private Long type;
 
     @ApiModelProperty(value = "cron表达式(日考评:秒 分 时 ? * 1-7  周考评:秒 分 时 ? * 周日  月考评:秒 时 分 日 * ?)")
     private String cronExpression;
