269f4e733cc134e030206e51b68ff9a6b2bea192
frank <Frank.han@cf688.net>
2019-10-21 12:42:35 +0800

    考评计划

--- crs-admin/src/main/java/com/cf/crs/entity/CheckPlan.java
+++ crs-admin/src/main/java/com/cf/crs/entity/CheckPlan.java
@@ -18,7 +18,7 @@ public class CheckPlan implements Serializable {
     @ApiModelProperty(value = "1:日报表，2:周报表，3:月报表 ,4:年报表")
     private Long type;
 
-    @ApiModelProperty(value = "cron表达式(日考评:秒 分 时 ? * 1-7  周考评:秒 分 时 ? * 周日  月考评:秒 时 分 日 * ?)")
+    @ApiModelProperty(value = "cron表达式(日考评:秒 分 时 ? * 1-7 *  周考评:秒 分 时 ? * 周日 *  月考评:秒 时 分 日 * ? *)")
     private String cronExpression;
 
     @ApiModelProperty(value = "发送邮箱")
