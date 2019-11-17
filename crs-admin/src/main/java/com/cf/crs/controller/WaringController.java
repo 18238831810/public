562305646f712fe5e2f4b03af8283ac51e9da617
frank <1527583922@qq.com>
2019-11-17 22:45:54 +0800

    考评模型

--- crs-admin/src/main/java/com/cf/crs/controller/WaringController.java
+++ crs-admin/src/main/java/com/cf/crs/controller/WaringController.java
@@ -1,19 +1,14 @@
 package com.cf.crs.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.cf.crs.entity.CheckMode;
-import com.cf.crs.service.CheckModeService;
 import com.cf.crs.service.WaringService;
 import com.cf.util.http.ResultJson;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 考评计划
