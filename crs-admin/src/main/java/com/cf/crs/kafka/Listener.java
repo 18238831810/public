bb07a24062be863548b9216825fc966df700b690
frank <frank.han@cf688.net>
2019-11-25 18:20:55 +0800

    kakfa

---
+++ crs-admin/src/main/java/com/cf/crs/kafka/Listener.java
@@ -0,0 +1,89 @@
+package com.cf.crs.kafka;
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.cf.crs.common.redis.RedisUtils;
+import com.cf.crs.entity.CityCar;
+import com.cf.crs.mapper.CityCarMapper;
+import com.cf.util.utils.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONArray;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class Listener {
+
+
+    @Autowired
+    RedisUtils redisUtils;
+
+    @Autowired
+    CityCarMapper cityCarMapper;
+
+
+    @KafkaListener(topics = "#{'${topic.lh}'}")
+    private void lhBatchListener(List<ConsumerRecord<String, Object>> consumerRecords, Acknowledgment acknowledgment){
+        //同步数据
+        insert(consumerRecords,list -> cityCarMapper.lhBatchInsert(list));
+    }
+
+    @KafkaListener(topics = "#{'${topic.hw}'}")
+    private void hwBatchListener(List<ConsumerRecord<String, Object>> consumerRecords, Acknowledgment acknowledgment){
+        //同步数据
+        insert(consumerRecords,list -> cityCarMapper.hwBatchInsert(list));
+    }
+
+    @KafkaListener(topics = "#{'${topic.zf}'}")
+    private void zfBatchListener(List<ConsumerRecord<String, Object>> consumerRecords, Acknowledgment acknowledgment){
+        //同步数据
+        insert(consumerRecords,list -> cityCarMapper.zfBatchInsert(list));
+    }
+
+    private void insert(List<ConsumerRecord<String, Object>> consumerRecords, Consumer<List<CityCar>> consumer) {
+        consumerRecords.forEach(record -> {
+            Object value = record.value();
+            List<Object> jsonArray = (List) JSONArray.fromObject(value);
+            List<CityCar> list = jsonArray.stream().filter(json -> {
+                JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(json));
+                if (StringUtils.isEmpty(jsonObject.getString("GPS_SJ"))) return false;
+                if (jsonObject.getInteger("GPS_ZT") == null) return false;
+                return true;
+            }).map(json -> {
+                JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(json));
+                CityCar cityCar = new CityCar();
+                cityCar.setRfid_id(jsonObject.getString("RFID_ID"));
+                cityCar.setDay(jsonObject.getString("GPS_SJ").substring(0, 10));
+                cityCar.setCp_hm(jsonObject.getString("CP_HM"));
+                cityCar.setJd(jsonObject.getDouble("JD"));
+                cityCar.setWd(jsonObject.getDouble("WD"));
+                cityCar.setSd(jsonObject.getString("SD"));
+                cityCar.setFx(jsonObject.getString("FX"));
+                cityCar.setGps_sj(jsonObject.getString("GPS_SJ"));
+                cityCar.setGps_sj_long(DateUtil.parseDate(jsonObject.getString("GPS_SJ_LONG"), DateUtil.TIMESTAMP));
+                cityCar.setLast_time(DateUtil.parseDate(jsonObject.getString("GPS_SJ_LONG"), DateUtil.TIMESTAMP));
+                cityCar.setGps_zt(jsonObject.getInteger("GPS_ZT"));
+                cityCar.setMatch(jsonObject.getInteger("MATCH"));
+                cityCar.setStatus(jsonObject.getString("GPS_SJ") + "#" + jsonObject.getInteger("GPS_ZT") + ",");
+                cityCar.setTotalTime(0L);
+                if (cityCar.getGps_zt() == 0)
+                    cityCar.setTotalTime(cityCar.getGps_sj_long() - DateUtil.getStartTime(cityCar.getGps_sj_long()).getTime());
+                return cityCar;
+            }).collect(Collectors.toList());
+            if (CollectionUtil.isEmpty(list)) return;
+            consumer.accept(list);
+        });
+    }
+
+}
+
