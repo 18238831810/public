111d6988cbc659d74b9a4d6de20bf48284d4ca67
frank <1527583922@qq.com>
2020-02-16 17:20:13 +0800

    编辑考评对象

--- crs-admin/src/main/java/com/cf/crs/service/CheckInfoService.java
+++ crs-admin/src/main/java/com/cf/crs/service/CheckInfoService.java
@@ -2,6 +2,7 @@ package com.cf.crs.service;
 
 import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.cf.crs.entity.CheckInfo;
 import com.cf.crs.entity.CheckMenu;
 import com.cf.crs.mapper.CheckInfoMapper;
@@ -30,7 +31,7 @@ public class CheckInfoService {
     CheckInfoMapper checkInfoMapper;
 
     /**
-     * 获取考评对象配置
+     * 获取对象信息
      * @return
      */
     public ResultJson<List<CheckInfo>> getCheckInfo(){
@@ -50,23 +51,21 @@ public class CheckInfoService {
     }
 
     /**
-     * 获取考评任务配置
+     * 新增考评对象信息
      * @return
-     *//*
-    public ResultJson<List<CheckInfo>> getCheckInfo(){
-        List<CheckInfo> list = checkInfoMapper.selectList(new QueryWrapper<CheckInfo>());
-        List<CheckInfo> result = Lists.newArrayList();
-        for (CheckInfo checkInfo : list) {
-            //先获取考评对象
-            if (checkInfo.getParentId() == 0 && checkInfo.getType() == 0){
-                Long id = checkInfo.getId();
-                //获取考评对象所属设备
-                Map<Integer, List<CheckInfo>> map = list.stream().filter(checkDevice -> (checkDevice.getParentId() != 0 || checkDevice.getType() != 0) && (long) checkDevice.getParentId() == id).collect(Collectors.groupingBy(CheckInfo::getType));
-                checkInfo.setDeviceList(map);
-                result.add(checkInfo);
-            }
-        }
-        return HttpWebResult.getMonoSucResult(result);
-    }*/
+     */
+    public ResultJson<List<String>> addCheckInfo(CheckInfo checkInfo){
+        checkInfo.setParentId(0);
+        checkInfo.setType(0);
+        return HttpWebResult.getMonoSucResult(checkInfoMapper.insert(checkInfo));
+    }
+    /**
+     * 修改考评对象信息
+     * @return
+     */
+    public ResultJson<List<String>> updateCheckInfo(CheckInfo checkInfo){
+        return HttpWebResult.getMonoSucResult(checkInfoMapper.update(null, new UpdateWrapper<CheckInfo>().eq("id", checkInfo.getId()).
+                set("name", checkInfo.getName()).set("displayName", checkInfo.getDisplayName()).set("email", checkInfo.getEmail()).set("informationSecurity", checkInfo.getInformationSecurity()).set("automatic", checkInfo.getAutomatic())));
+    }
 
 }
