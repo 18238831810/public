c2cbad83ccdabffaff31ca9cb19d53ef03225078
frank.han <frank.han@cf688.net>
2020-04-29 13:01:30 +0800

    第三方登录

--- crs-common/src/main/java/com/cf/crs/common/config/ResttemplateConfig.java
+++ crs-common/src/main/java/com/cf/crs/common/config/ResttemplateConfig.java
@@ -1,21 +1,19 @@
 package com.cf.crs.common.config;
 
-import org.apache.http.config.SocketConfig;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * @author frank
@@ -24,19 +22,39 @@ import java.security.cert.X509Certificate;
 @Configuration
 public class ResttemplateConfig {
 
+    private static final HostnameVerifier PROMISCUOUS_VERIFIER = (s, sslSession ) -> true;
+
 
     @Bean
-    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).setDefaultSocketConfig(SocketConfig.custom().setSoTimeout(60000).build()).build();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        requestFactory.setReadTimeout(60000);
-        requestFactory.setConnectTimeout(5000);
-        requestFactory.setConnectionRequestTimeout(5000);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
+    public SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory() {
+            @Override
+            protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
+                if (connection instanceof HttpsURLConnection) {
+                    ((HttpsURLConnection) connection).setHostnameVerifier(PROMISCUOUS_VERIFIER);
+                }
+                super.prepareConnection(connection, httpMethod);
+            }
+        };
+        simpleClientHttpRequestFactory.setReadTimeout(10000);
+        simpleClientHttpRequestFactory.setConnectTimeout(5000);
+        return simpleClientHttpRequestFactory;
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(simpleClientHttpRequestFactory());
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler());
+        //将原来的StringHttp字符编码改成utf-8
+        List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof StringHttpMessageConverter) {
+                ((StringHttpMessageConverter) list.get(i)).setDefaultCharset(Charset.forName("utf-8"));
+                break;
+            }
+        }
         return restTemplate;
     }
 }
